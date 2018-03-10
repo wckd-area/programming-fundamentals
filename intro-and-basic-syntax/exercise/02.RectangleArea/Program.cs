0d50d69f41e7f6b6c377c3354672d0b0398bd400
ekaterin <ekaterinkostadinov@gmail.com>
2018-03-10 23:13:48 +0200

    adding solution of problem 2

---
+++ intro-and-basic-syntax/exercise/02.RectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2.RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double result = width * height;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
