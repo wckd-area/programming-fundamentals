6fda054fbc704ccc2e31a790ce8ea538311865af
aquariuz <gpetiaa@gmail.com>
2018-03-11 17:48:36 +0200

    Documented problem 2.

--- intro-and-basic-syntax/exercise/02.RectangleArea/Program.cs
+++ intro-and-basic-syntax/exercise/02.RectangleArea/Program.cs
@@ -7,9 +7,14 @@ namespace _2.RectangleArea
     {
         static void Main(string[] args)
         {
+            // naming the double variables and reading from the console
             double rectangleWidth = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+
+            // calculating rectangle area
             double result = rectangleWidth * height;
+
+            // printing out output in correct format
             Console.WriteLine($"{result:f2}");
         }
     }
