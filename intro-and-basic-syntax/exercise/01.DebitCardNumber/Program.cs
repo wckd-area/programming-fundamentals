5e536f6007dd846defb9d0b7bf34a0e006efc8ad
myilkova <miroslava.y.ilkova@gmail.com>
2018-03-11 11:40:01 +0200

    Updated 01.DebitCardNumber/Program.CS: changed the name of the first var: firstNumber -> FirstNum and compiled. /bin/ and /obj/ are also automatically updated

--- intro-and-basic-syntax/exercise/01.DebitCardNumber/Program.cs
+++ intro-and-basic-syntax/exercise/01.DebitCardNumber/Program.cs
@@ -6,12 +6,12 @@ namespace Debit_Card_Number
     {
         static void Main(string[] args)
         {
-            var firstNumber = int.Parse(Console.ReadLine());
+            var firstNum = int.Parse(Console.ReadLine());
             var secondNumber = int.Parse(Console.ReadLine());
             var thirdNumber = int.Parse(Console.ReadLine());
             var forthNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{firstNumber:d4} {secondNumber:d4} {thirdNumber:d4} {forthNumber:d4}");
+            Console.WriteLine($"{firstNum:d4} {secondNumber:d4} {thirdNumber:d4} {forthNumber:d4}");
         }
     }
 }
