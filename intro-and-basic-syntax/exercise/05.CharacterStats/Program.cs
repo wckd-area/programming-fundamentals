052a17edc7b38a738460119eb46e2dbe64935e53
ekaterin <ekaterinkostadinov@gmail.com>
2018-03-11 15:12:05 +0200

    Documented The code of Problem 5

--- intro-and-basic-syntax/exercise/05.CharacterStats/Program.cs
+++ intro-and-basic-syntax/exercise/05.CharacterStats/Program.cs
@@ -6,15 +6,16 @@ namespace CharacterStats
     {
         static void Main(string[] args)
         {
+		// we are reading some variables
             var nameOfPerson = Console.ReadLine();
             var currentHealth = int.Parse(Console.ReadLine());
             var maximumHealth = int.Parse(Console.ReadLine());
             var currentEnergy = int.Parse(Console.ReadLine());
             var maximumEnergy = int.Parse(Console.ReadLine());
-
+		// concatenating strings
             var health = new string('|', currentHealth) + new string('.', maximumHealth - currentHealth);
             var energy = new string('|', currentEnergy) + new string('.', maximumEnergy - currentEnergy);
-
+		//printing result
             Console.WriteLine("Name: {0}", name);
             Console.WriteLine("Health: |" + health + "|");
             Console.WriteLine("Energy: |" + energy + "|");
